d58ad6a3a55eb80cfa6c9be2a4e569b8401e00a8
Felix <87980522+Xileff@users.noreply.github.com>
2022-10-25 10:06:39 +0700

    Get Single Book Detail

--- src/handler.js
+++ src/handler.js
@@ -91,4 +91,31 @@ const getAllBooksHandler = (request, h) => {
   return response;
 };
 
-module.exports = { addBookHandler, getAllBooksHandler };
+const getBookDetail = (request, h) => {
+  const { bookId } = request.params;
+  const index = books.findIndex((b) => b.id === bookId);
+
+  // Case : Buku tidak ditemukan
+  if (index === -1) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Buku tidak ditemukan',
+    });
+
+    response.code(404);
+    return response;
+  }
+
+  // Case : Buku ditemukan
+  const response = h.response({
+    status: 'success',
+    data: {
+      book: books[index],
+    },
+  });
+
+  response.code(200);
+  return response;
+};
+
+module.exports = { addBookHandler, getAllBooksHandler, getBookDetail };
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,25 @@
-const { addBookHandler, getAllBooksHandler } = require('./handler');
+const { addBookHandler, getAllBooksHandler, getBookDetail } = require('./handler');
 
 const routes = [
+  // Add book
   {
     method: 'POST',
     path: '/books',
     handler: addBookHandler,
   },
+
+  // Get all books
   {
     method: 'GET',
     path: '/books',
     handler: getAllBooksHandler,
+  },
+
+  // Get single book detail
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBookDetail,
   },
 ];
 
